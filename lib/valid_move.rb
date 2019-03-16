645c288364e866edf102a088b2c70cbfe6b3d2d3
BEAU WITKA <bwitka@gmail.com>
2019-03-16 03:18:42 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 # code your #valid_move? method here
 
 def valid_move?(board, index)
-  if position_taken? == false && index.between?
+  if position_taken? == false && index.between?(1,9)
     true
   else
     false
