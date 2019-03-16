ee54b487a4261a48229f5cf8b8b3cdb2d636960a
BEAU WITKA <bwitka@gmail.com>
2019-03-16 03:10:11 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 # code your #valid_move? method here
 
 def valid_move?(array, index_number)
-  if position_taken?(array, index_number) == false && array.include?(index_number)
+  if position_taken?
     true
   end
 end
